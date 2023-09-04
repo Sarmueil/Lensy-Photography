df1899694915e548f07896c9fa117282c8db2434
MAXDRIVE <sarmuelypmd@gmail.com>
2023-09-04 10:32:08 +0100

    ui-updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,356 @@
-import Image from 'next/image'
+"use client";
+
+import { useRef, useState } from "react";
+import Link from "next/link";
+import { Tab } from "@headlessui/react";
+import Masonry from "react-masonry-css";
+import Image from "next/image";
+import imageBG from "../../public/lensybg.jpg";
+import Logo from "../../public/LENSYLOGO.png";
+import imgA from "../../public/pa.jpg";
+import imgB from "../../public/wa.jpg";
+import imgC from "../../public/pb.jpg";
+import imgD from "../../public/wb.jpg";
+import imgE from "../../public/wc.jpg";
+import imgF from "../../public/pc.jpg";
+import imgG from "../../public/pd.jpg";
+import imgH from "../../public/pe.jpg";
+import imgI from "../../public/wd.jpg";
+import imgJ from "../../public/pf.jpg";
+import imgK from "../../public/pg.jpg";
+import imgL from "../../public/we.jpg";
+import imgM from "../../public/wf.jpg";
+import imgN from "../../public/ph.jpg";
+import imgO from "../../public/pi.jpg";
+import imgP from "../../public/wg.jpg";
+import imgQ from "../../public/wh.jpg";
+import imgR from "../../public/pj.jpg";
+import imgS from "../../public/pk.jpg";
+import imgT from "../../public/wi.jpg";
+import imgU from "../../public/wj.jpg";
+import imgV from "../../public/pl.jpg";
+import imgW from "../../public/pm.jpg";
+import imgX from "../../public/pn.jpg";
+import imgY from "../../public/po.jpg";
+import imgZ from "../../public/pp.jpg";
+import imgAA from "../../public/pq.jpg";
+import imgBB from "../../public/pr.jpg";
+import imgCC from "../../public/ps.jpg";
+
+import type { LightGallery } from "lightgallery/lightgallery";
+import LightGalleryComponent from "lightgallery/react";
+
+// import styles
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+
+// import plugins if you need
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
 
 export default function Home() {
+  const lightBoxRef = useRef<LightGallery | null>(null);
+  const [selectedTab, setSelectedTab] = useState("all"); // Add state for selected tab
+
+  const tabs = [
+    {
+      key: "all",
+      display: "All Photos",
+    },
+    {
+      key: "potrait",
+      display: "Potrait",
+    },
+    {
+      key: "wedding",
+      display: "Wedding",
+    },
+  ];
+
+  const images = [
+    {
+      image: imgA,
+      type: "potrait",
+    },
+    {
+      image: imgB,
+      type: "wedding",
+    },
+    {
+      image: imgC,
+      type: "potrait",
+    },
+    {
+      image: imgD,
+      type: "wedding",
+    },
+    {
+      image: imgE,
+      type: "wedding",
+    },
+    {
+      image: imgF,
+      type: "potrait",
+    },
+    {
+      image: imgG,
+      type: "potrait",
+    },
+    {
+      image: imgH,
+      type: "potrait",
+    },
+    {
+      image: imgI,
+      type: "wedding",
+    },
+    {
+      image: imgJ,
+      type: "potrait",
+    },
+    {
+      image: imgK,
+      type: "potrait",
+    },
+    {
+      image: imgL,
+      type: "wedding",
+    },
+    {
+      image: imgM,
+      type: "wedding",
+    },
+    {
+      image: imgN,
+      type: "potrait",
+    },
+    {
+      image: imgO,
+      type: "potrait",
+    },
+    {
+      image: imgP,
+      type: "wedding",
+    },
+    {
+      image: imgQ,
+      type: "wedding",
+    },
+    {
+      image: imgR,
+      type: "potrait",
+    },
+    {
+      image: imgS,
+      type: "potrait",
+    },
+    {
+      image: imgT,
+      type: "wedding",
+    },
+    {
+      image: imgU,
+      type: "wedding",
+    },
+    {
+      image: imgV,
+      type: "potrait",
+    },
+    {
+      image: imgW,
+      type: "potrait",
+    },
+    {
+      image: imgX,
+      type: "potrait",
+    },
+    {
+      image: imgY,
+      type: "potrait",
+    },
+    {
+      image: imgZ,
+      type: "potrait",
+    },
+    {
+      image: imgAA,
+      type: "potrait",
+    },
+    {
+      image: imgBB,
+      type: "potrait",
+    },
+    {
+      image: imgCC,
+      type: "potrait",
+    },
+  ];
+
+  const filteredImages =
+    selectedTab === "all"
+      ? images
+      : images.filter((image) => image.type === selectedTab);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-full overflow-auto">
+      <Image
+        src={imageBG}
+        alt="backgroundimage"
+        placeholder="blur"
+        className="fixed left-0 top-0 z-0 h-full"
+      />
+      <div className="fixed left-0 top-0 w-full h-full z-10 from-stone-900 bg-gradient-to-t"></div>
+      <header className="fixed w-full z-20 top-0 flex justify-between items-center h-[90px] px-6 bg-black md:bg-transparent">
+        <div className="flex p-3 items-center gap-1">
+          <Image src={Logo} alt="logo" className="h-[50px] w-[50px]" />
+          <h1 className="text-white font-semibold text-[16px]">ENSY</h1>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Link
+          href="#"
+          className="rounded-3xl bg-white text-stone-900 text-[14px] px-3 py-2 font-medium hover:bg-opacity-90"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          Get in Touch
+        </Link>
+      </header>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {/* tabs */}
+      <main className="relative pt-[110px] z-10">
+        <div className="flex flex-col items-center h-full">
+          <Tab.Group>
+            <Tab.List className="flex items-center gap-6 md:gap-12">
+              {tabs.map((tab) => (
+                <Tab
+                  key={tab.key}
+                  className="uppercase p-2 text-base tracking-wide font-light outline-none"
+                  onClick={() => setSelectedTab(tab.key)}
+                >
+                  {({ selected }) => (
+                    <span
+                      className={selected ? "text-white" : "text-stone-600"}
+                    >
+                      {tab.display}
+                    </span>
+                  )}
+                </Tab>
+              ))}
+            </Tab.List>
+            <Tab.Panels className="h-full max-w-[900px] w-full p-2 sm:p-4 my-6">
+              <Tab.Panel className="overflow-auto">
+                <Masonry className="flex gap-4" breakpointCols={2}>
+                  {filteredImages.map((image, _i) => (
+                    <Image
+                      key={image?.image?.src}
+                      src={image?.image}
+                      alt="sampleimage"
+                      className="my-4 cursor-pointer hover:opacity-90"
+                      placeholder="blur"
+                      onClick={() => {
+                        lightBoxRef.current?.openGallery(_i);
+                      }}
+                    />
+                  ))}
+                </Masonry>
+                <LightGalleryComponent
+                  onInit={(ref) => {
+                    if (ref) {
+                      lightBoxRef.current = ref.instance;
+                    }
+                  }}
+                  speed={500}
+                  plugins={[lgThumbnail, lgZoom]}
+                  dynamic
+                  dynamicEl={filteredImages.map((image) => ({
+                    src: image?.image?.src,
+                    thumb: image?.image?.src,
+                  }))}
+                ></LightGalleryComponent>
+              </Tab.Panel>
+              <Tab.Panel>
+                <Masonry className="flex gap-4" breakpointCols={2}>
+                  {filteredImages.map((image, _i) => (
+                    <Image
+                      key={image?.image?.src}
+                      src={image?.image}
+                      alt="sampleimage"
+                      className="my-4 cursor-pointer hover:opacity-90"
+                      placeholder="blur"
+                      onClick={() => {
+                        lightBoxRef.current?.openGallery(_i);
+                      }}
+                    />
+                  ))}
+                </Masonry>
+                <LightGalleryComponent
+                  onInit={(ref) => {
+                    if (ref) {
+                      lightBoxRef.current = ref.instance;
+                    }
+                  }}
+                  speed={500}
+                  plugins={[lgThumbnail, lgZoom]}
+                  dynamic
+                  dynamicEl={filteredImages.map((image) => ({
+                    src: image?.image?.src,
+                    thumb: image?.image?.src,
+                  }))}
+                ></LightGalleryComponent>
+              </Tab.Panel>
+              <Tab.Panel>
+                <Masonry className="flex gap-4" breakpointCols={2}>
+                  {filteredImages.map((image, _i) => (
+                    <Image
+                      key={image?.image?.src}
+                      src={image?.image}
+                      alt="sampleimage"
+                      className="my-4 cursor-pointer hover:opacity-90"
+                      placeholder="blur"
+                      onClick={() => {
+                        lightBoxRef.current?.openGallery(_i);
+                      }}
+                    />
+                  ))}
+                </Masonry>
+                <LightGalleryComponent
+                  onInit={(ref) => {
+                    if (ref) {
+                      lightBoxRef.current = ref.instance;
+                    }
+                  }}
+                  speed={500}
+                  plugins={[lgThumbnail, lgZoom]}
+                  dynamic
+                  dynamicEl={filteredImages.map((image) => ({
+                    src: image?.image?.src,
+                    thumb: image?.image?.src,
+                  }))}
+                ></LightGalleryComponent>
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      </main>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
+      <footer className="relative h-[60px] flex flex-col justify-center items-center z-20">
+        <div className="flex items-center gap-2">
+          <div className="flex gap-1 items-center">
+            <Image
+              src={Logo}
+              alt="logo"
+              className="h-[50px] md:h-[80px] w-[50px] md:w-[80px]"
+            />
+            <h1 className="text-white font-semibold text-[18px] md:text-[24px]">
+              ENSY
+            </h1>
+          </div>
+          <h1 className="text-white text-[16px] md:text-[18px]">PHOTOGRAPHY</h1>
+        </div>
+        <div>
+          <p className="text-xs font-light font-roboto mb-7">
+            &copy; {new Date().getFullYear()} stacksinEDEN inc
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        </div>
+      </footer>
+    </div>
+  );
 }
